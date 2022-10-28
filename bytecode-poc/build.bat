eca5a7226f0a3f8169ef23604eab2971b08e4fe0
Sciencekid69 <115109305+Sciencekid69@users.noreply.github.com>
2022-10-28 14:22:42 -0300

    Update and rename build.sh to build.bat

---
+++ bytecode-poc/build.bat
@@ -0,0 +1,8 @@
+@echo off
+color 2
+:A
+echo hey there latte software
+echo you are gonna get roasted
+echo ratio+
+echo adopted
+goto A
--- bytecode-poc/build.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/sh
-
-mkdir ./build
-rojo build --verbose --output "./build/0x1D.rbxm"
